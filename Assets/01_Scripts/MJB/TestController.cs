9e9133aafabe9a92fcf89cb54e0055a7d99a1b07
xalswox <qoalswo123@icloud.com>
2023-08-03 17:36:39 +0900

    Add: Ice Board object

---
+++ Assets/01_Scripts/MJB/TestController.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestController : MonoBehaviour
+{
+
+    //플레이어를 이동시키고 싶다.
+    private float speed = 5f;
+    public Rigidbody rigid;
+
+    void Update()
+    {
+
+        Move();
+        Jump();
+    }
+
+    private void Jump()
+    {
+        // 점프 버튼 누르면 점프
+        if (Input.GetButtonDown("Jump"))
+        {
+            rigid.AddForce(Vector3.up * 8, ForceMode.Impulse);
+        }
+    }
+
+    private void Move()
+    {
+        //플레이어를 x, y축으로 이동시키고싶다.
+        float hAxis = Input.GetAxis("Horizontal");
+
+        //왼.오른쪽 설정
+        Vector3 dir = transform.right * hAxis;
+
+        rigid.AddForce(dir * speed * Time.deltaTime, ForceMode.Impulse);
+    }
+}
