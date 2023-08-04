d214c49338e91f465a74db574dd6bfafd5331da6
hhkim331 <97862258+hhkim331@users.noreply.github.com>
2023-08-04 17:52:38 +0900

    partybox

---
+++ Assets/01_Scripts/KHH/PartyBox.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PartyBox : MonoBehaviour
+{
+    TotalStageObjectData totalStageObjectData;
+    List<StageObject> stageObjects;
+
+    [Header("자리배치")]
+    [SerializeField] Transform[] fourSeats;
+    [SerializeField] Transform[] fiveSeats;
+    [SerializeField] Transform[] sixSeats;
+
+    [Header("연출용")] //나중에 애니메이터로 바꿔야함
+    [SerializeField] Transform topLeft;
+    [SerializeField] Transform topRight;
+    [SerializeField] Transform topBlankLeft;
+    [SerializeField] Transform topBlankRight;
+
+    public void Init()
+    {
+        totalStageObjectData = Resources.Load<TotalStageObjectData>("ScriptableObject/TotalStageObjectData");
+        stageObjects = new List<StageObject>();
+    }
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+
+    //}
+
+    public void SetBox()
+    {
+        int randCount = Random.Range(4, 7);
+        for (int i = 0; i < randCount; i++)
+        {
+            int randItem = Random.Range(0, totalStageObjectData.stageObjectDataList.Count);
+            GameObject go = Instantiate(totalStageObjectData.stageObjectDataList[randItem], transform);
+            go.transform.localScale = Vector3.one * 0.5f;
+            //자리배치
+            if (randCount == 4)
+                go.transform.localPosition = fourSeats[i].localPosition;
+            else if (randCount == 5)
+                go.transform.localPosition = fiveSeats[i].localPosition;
+            else if (randCount == 6)
+                go.transform.localPosition = sixSeats[i].localPosition;
+
+            StageObject stageObject = go.GetComponent<StageObject>();
+            stageObjects.Add(stageObject);
+        }
+    }
+
+    public void Open()
+    {
+        gameObject.SetActive(true);
+        topLeft.gameObject.SetActive(false);
+        topRight.gameObject.SetActive(false);
+        topBlankLeft.gameObject.SetActive(false);
+        topBlankRight.gameObject.SetActive(false);
+
+        KHHGameManager.instance.cursor.Active();
+        //이건 애니메이터로 하는게 맞는듯
+    }
+
+    public void Close()
+    {
+        gameObject.SetActive(false);
+
+        //이건 애니메이터로 하는게 맞는듯
+
+        //남은 오브젝트 제거
+        RemoveItemAll();
+    }
+
+    /// <summary>
+    /// 선택된 아이템 리스트에서 제거
+    /// </summary>
+    public void RemoveItem(StageObject stageObject)
+    {
+        stageObjects.Remove(stageObject);
+    }
+
+    /// <summary>
+    /// 리스트 전체 제거
+    /// </summary>
+    void RemoveItemAll()
+    {
+        foreach (var item in stageObjects)
+        {
+            Destroy(item.gameObject);
+        }
+        stageObjects.Clear();
+    }
+}
--- Assets/01_Scripts/KHH/StageObject.cs
+++ Assets/01_Scripts/KHH/StageObject.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using DG.Tweening;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,7 +8,11 @@ public class StageObject : MonoBehaviour
     public StageObjectData objectData;
 
     public Transform cursor;    //현재 나를 잡고 있는 커서
+    bool isFocus = false;   //현재 커서가 나를 잡고 있는지 여부
     bool isPlace = false;   //배치되었는지 여부
+
+    public Transform meshTransform; //스케일 조정을 위한 현재 오브젝트의 메쉬
+    Vector3 meshDefaultScale;   //메쉬의 기본 스케일
 
     // Start is called before the first frame update
     void Start()
@@ -21,42 +26,72 @@ public class StageObject : MonoBehaviour
                         objectData.objectTileList.Add(new Vector2(i, j));
             }
         }
+
+        if (meshTransform != null)
+            meshDefaultScale = meshTransform.localScale;
     }
 
-    // Update is called once per frame
-    void Update()
+    //// Update is called once per frame
+    //void Update()
+    //{
+    //    if (objectData.objectType == StageObjectData.ObjectType.Fixed) return;
+
+    //    if (KHHGameManager.instance.state == KHHGameManager.GameState.Select)
+    //    {
+    //        //Select();
+    //    }
+    //    else if (KHHGameManager.instance.state == KHHGameManager.GameState.Place && !isPlace)
+    //    {
+    //        //Place();
+    //        if (isPlace == false)
+    //        {
+    //            Move();
+    //        }
+    //    }
+    //}
+
+    /// <summary>
+    /// 포커스
+    /// </summary>
+    public void Focus(bool isFocus)
     {
-        if (objectData.objectType == StageObjectData.ObjectType.Fixed) return;
-
-        if (isPlace == false)
+        this.isFocus = isFocus;
+        if (isFocus)
         {
-            Move();
-            if (Input.GetKeyDown(KeyCode.X))   //배치
-            {
-                isPlace = true;
-                Place();
-            }
-
-            if (Input.GetKeyDown(KeyCode.C))    //회전
-            {
-                switch (objectData.objectRotType)
-                {
-                    case StageObjectData.ObjectRotType.Spin:
-                        Spin();
-                        break;
-                    case StageObjectData.ObjectRotType.FlipY:
-                        FlipY();
-                        break;
-                }
-            }
+            transform.DOKill();
+            if (meshTransform != null)
+                meshTransform.DOScale(meshDefaultScale * 1.2f, 0.3f);
+        }
+        else
+        {
+            transform.DOKill();
+            if (meshTransform != null)
+                meshTransform.DOScale(meshDefaultScale, 0.3f);
         }
     }
 
-    public void Move()  //이동
+    /// <summary>
+    /// 선택
+    /// </summary>
+    public void Select(Transform cursorTr)
     {
-        Vector2 cursorPos = Camera.main.ScreenToWorldPoint(cursor.position);
-        Vector2 newPos;
+        cursor = cursorTr;
+        transform.parent = null;
+        transform.localScale = Vector3.one;
+        gameObject.SetActive(false);
 
+        isFocus = false;
+        //크기 애니메이션 상태 초기화
+
+        KHHGameManager.instance.partyBox.RemoveItem(this);
+    }
+
+    /// <summary>
+    /// 이동
+    /// </summary>
+    public void Move(Vector2 cursorPos)
+    {
+        Vector2 newPos;
         //소숫점이 0.5이상이면 올림, 0.5미만이면 내림
         if (cursorPos.x - (int)cursorPos.x >= 0.5f)
             newPos.x = (int)cursorPos.x + 1;
@@ -71,7 +106,26 @@ public class StageObject : MonoBehaviour
         transform.position = newPos - objectData.objectPickUpPos;
     }
 
-    public void Spin()  //회전
+    /// <summary>
+    /// 회전
+    /// </summary>
+    public void Rotate()
+    {
+        switch (objectData.objectRotType)
+        {
+            case StageObjectData.ObjectRotType.Spin:
+                Spin();
+                break;
+            case StageObjectData.ObjectRotType.FlipY:
+                FlipY();
+                break;
+        }
+    }
+
+    /// <summary>
+    /// 오른쪽 90도 회전
+    /// </summary>
+    void Spin()
     {
         for (int i = 0; i < objectData.objectTileList.Count; i++)
         {
@@ -86,30 +140,34 @@ public class StageObject : MonoBehaviour
         transform.Rotate(new Vector3(0, 0, -90));
     }
 
-    public void FlipY() //Y축 뒤집기
+    /// <summary>
+    /// Y축 뒤집기
+    /// </summary>
+    void FlipY()
     {
         for (int i = 0; i < objectData.objectTileList.Count; i++)
         {
-            //회전축을 기준으로 좌표를 오른쪽 90도 회전 좌표로 변경
             Vector2 tilePos = objectData.objectTileList[i] - objectData.objectRotAxis;
 
-            //tilePos를 오른쪽 90도 회전
             Vector2 newTilePos = new Vector2(-tilePos.x, tilePos.y);
             objectData.objectTileList[i] = newTilePos + objectData.objectRotAxis;
         }
 
-        transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+        //y축 180도 회전
+        transform.Rotate(new Vector3(0, 180, 0));
     }
 
-    //오브젝트 배치
+    /// <summary>
+    /// 오브젝트 배치
+    /// </summary>
     public void Place()
     {
+        isPlace = true;
         for (int i = 0; i < objectData.objectTileList.Count; i++)
         {
             Vector2 tilePos = new Vector2(transform.position.x + objectData.objectLeftBottomPos.x + objectData.objectTileList[i].x,
                 transform.position.y + objectData.objectLeftBottomPos.y + objectData.objectTileList[i].y);
             MapManager.instance.mapObjectDic[tilePos] = this;
-            Debug.Log(tilePos);
         }
     }
 }
