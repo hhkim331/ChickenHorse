c8cd8e2c1798dcf0850bedd892689048c9bf7a4d
xalswox <qoalswo123@icloud.com>
2023-08-04 17:52:19 +0900

    Add: Arrow & ArrowShooter object
    애니메이션 발사할 때 화살 발사

---
+++ Assets/01_Scripts/MJB/Arrow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow : MonoBehaviour
+{
+    // 화살을 날리고 싶다.
+    // 속력
+    public float speed = 5f;
+    void Update()
+    {
+        // 위의 방향으로
+        transform.position += transform.up * speed * Time.deltaTime;
+    }
+    //충돌하려는 물체가
+    void OnCollisionEnter(Collision collision)
+    {
+        Destroy(gameObject);
+        Destroy(collision.gameObject);
+    }
+}
---
+++ Assets/01_Scripts/MJB/ArrowFire.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowFire : MonoBehaviour
+{
+    // 화살 공장을 만들어서 발사한다.
+    // 발사 공장
+    public GameObject arrowFactory;
+    // 발사 위치
+    public Transform fireTransform;
+    void FireArrow()
+    {
+        Debug.Log("FireArrow");
+        // 화살 발사 공장을 가동한다.
+        GameObject arrow = Instantiate(arrowFactory);
+        // 발사 물체를 발사 위치에 놓는다.
+        arrow.transform.position = fireTransform.position;
+        // 발사 물체의 위 방향을 발사 위치의 위 방향이다.
+        arrow.transform.up = fireTransform.up;
+    }
+}
