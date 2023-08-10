e2eaaca6553f452beeb0bb342f54fb8c80eb71d4
xalswox <qoalswo123@icloud.com>
2023-08-10 16:52:21 +0900

    Add: Main Lobby Background image

---
+++ Assets/01_Scripts/MJB/ChangeCursor.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeCursor : MonoBehaviour
+{
+    //커서 이미지를 가져온다.
+    [SerializeField] private Texture2D cursorImage;
+
+    //시작할때 커서 이미지를 가져와서 변경한다.
+    private void Start()
+    {
+        Cursor.SetCursor(cursorImage, Vector2.zero, CursorMode.ForceSoftware);
+    }
+}
