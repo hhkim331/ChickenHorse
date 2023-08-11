fb6269896bce63ac2c996ef60d02c7f4766ce581
xalswox <qoalswo123@icloud.com>
2023-08-11 14:28:21 +0900

    Refactor: 특정 값 상수화
    문자열, 자연수, 실수 상수화

--- Assets/01_Scripts/MJB/StartGameManager.cs
+++ Assets/01_Scripts/MJB/StartGameManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
@@ -13,10 +14,12 @@ public class StartGameManager : MonoBehaviour
     public TextMeshProUGUI timerUI;
 
     //3초 시간이 제한
-    private readonly float limitTime = 0.5f;
+    private readonly float MAX_TIME = 3.4f, LIMIT_TIME = 0.5f;
 
-    //3초 시간이 되었다는 체크
-    //플레이어가 들어갔나요?
+    private readonly int NEXT_SCENE_NUMBER = 1;
+    private readonly string START_GAME_TEXT = "게임 시작까지\n", PLAYER_TAG_NAME = "Player", DEMICAL_POINT_TRUNCATION = "0";
+
+    //3초 시간이 되었다는 체크, 플레이어가 들어갔나요?
     private bool hasLimit, hasPlayer;
 
     //시간에 대한 접근
@@ -27,10 +30,17 @@ public class StartGameManager : MonoBehaviour
         //나 자신을 끈다.
         timerUI.enabled = false;
         // 처음 시간을 초기화
-        currentTime = 3.4f;
+        currentTime = MAX_TIME;
     }
 
     private void Update()
+    {
+        SetCountDown();
+        //일정 시간이 지난다면 씬을 바꾼다.
+        if (hasLimit) ChangeScene();
+    }
+
+    private void SetCountDown()
     {
         //플레이어가 있을때만 실행시킨다.
         if (hasPlayer)
@@ -38,30 +48,29 @@ public class StartGameManager : MonoBehaviour
             //시간을 증가시켜 문자열로 변환한다.
             currentTime -= Time.deltaTime;
             //만약에 시간이 1초가 되었다면?
-            if (currentTime < limitTime)
+            if (currentTime < LIMIT_TIME)
             {
                 //시간이 지났다는 체크를 한다.
                 hasLimit = true;
                 //시간을 3초로 초기화하고
-                currentTime = 3.4f;
+                currentTime = MAX_TIME;
             }
 
             SetTimerText();
         }
-        //일정 시간이 지난다면 씬을 바꾼다.
-        if (hasLimit) ChangeScene();
     }
 
     private void SetTimerText()
     {
+        string numberText = currentTime.ToString(DEMICAL_POINT_TRUNCATION);
         //UI에 시간을 넣는다.
-        timerUI.text = "게임 시작까지\n" + currentTime.ToString("0");
+        timerUI.text = START_GAME_TEXT + numberText;
     }
 
-    public void ChangeScene()
+    private void ChangeScene()
     {
         //만약에 일정시간이 된다면 다음씬으로 이동한다.
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene(NEXT_SCENE_NUMBER);
         //시간 UI 비활성화 한다.
         timerUI.enabled = false;
     }
@@ -69,7 +78,7 @@ public class StartGameManager : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         //플레이어 태그가 닿았다면
-        if (other.CompareTag("Player"))
+        if (other.CompareTag(PLAYER_TAG_NAME))
         {
             //플레이어가 들어가있다.
             hasPlayer = true;
@@ -82,14 +91,14 @@ public class StartGameManager : MonoBehaviour
     private void OnTriggerExit(Collider other)
     {
         //나 자신을 끈다.
-        if (other.CompareTag("Player"))
+        if (other.CompareTag(PLAYER_TAG_NAME))
         {
             //플레이어가 없다.
             hasPlayer = false;
             //시간UI 비활성화 한다.
             timerUI.enabled = false;
             //시간을 3초로 초기화 한다.
-            currentTime = 3.4f;
+            currentTime = MAX_TIME;
         }
     }
 }
