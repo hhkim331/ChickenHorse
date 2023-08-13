ec62c44c7aa633727958c8116a90c3ee946c8352
xalswox <qoalswo123@icloud.com>
2023-08-13 13:11:41 +0900

    Feat: 다중 타겟 카메라
    플레이어 간의 거리 중심에 따른 FOV 컨트롤

--- Assets/01_Scripts/MJB/ChangeCursor.cs
+++ Assets/01_Scripts/MJB/ChangeCursor.cs
@@ -9,7 +9,13 @@ public class ChangeCursor : MonoBehaviour
 
     public GameObject player;
 
-    //시작할때 커서 이미지를 가져와서 변경한다.
+    private void Awake()
+    {
+        //커서를 게임 뷰에 나가지 못하게 한다.
+        Cursor.lockState = CursorLockMode.Confined;
+    }
+
+    //시작하기 전에 커서 이미지를 가져와서 변경한다.
     private void Start()
     {
         Vector2 cursorPosition = new Vector2(cursorImage.width / 2, cursorImage.height / 2);
@@ -34,7 +40,8 @@ public class ChangeCursor : MonoBehaviour
             //플레이어의 컨트롤러를 끈다. // 플레이어의 컨트롤러 작업
             player.GetComponent<TestController>().enabled = false;
             Cursor.visible = true;
-            Cursor.lockState = CursorLockMode.None;
+            //커서를 게임 뷰에 나가지 못하게 한다.
+            Cursor.lockState = CursorLockMode.Confined;
         }
     }
 }
---
+++ Assets/01_Scripts/MJB/MultipleTargetCamera.cs
@@ -0,0 +1,87 @@
+﻿using Cinemachine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEngine.WSA;
+
+public class MultipleTargetCamera : MonoBehaviour
+{
+    //위치 리스트가 필요하다.
+    public List<Transform> targets;
+
+    //위치값을 조정하고 싶다.
+    public Vector2 offset;
+
+    //fov 최소, 최댓값
+    public float maxZoom = 12f, minZoom = 7f, limitZoom = 50f;
+
+    //해당 카메라를 담을 변수 생성
+    private CinemachineVirtualCamera virtualCam;
+
+    private void Awake()
+    {
+        virtualCam = this.GetComponent<CinemachineVirtualCamera>();
+    }
+
+    // 타겟의 중심점을 가져온다.
+    private void LateUpdate()
+    {
+        //타겟이 없을때는 함수를 빠져나간다.
+        if (targets.Count == 0) return;
+
+        Move();
+        Zoom();
+    }
+
+    private void Move()
+    {
+        Vector2 centerPosition = GetCenterPosition();
+
+        Vector2 newPosition = centerPosition + offset;
+        //카메라의 중심을 부드럽게 이동을 지정한다.
+        transform.position = newPosition;
+    }
+
+    private void Zoom()
+    {
+        //카메라 줌 값을 선형 보간한다.
+        float newZoom = Mathf.Lerp(minZoom, maxZoom, GetGreatestPosition() / limitZoom);
+        //그리고 메인 카메라의 보는 값과 타겟 줌 값을 선형 보간한다.
+        virtualCam.m_Lens.OrthographicSize = Mathf.Lerp(virtualCam.m_Lens.OrthographicSize, newZoom, Time.deltaTime);
+    }
+
+    private float GetGreatestPosition()
+    {
+        //bound로 거리를 만든다
+        var bounds = new Bounds(targets[0].position, Vector2.zero);
+        //모든 물체를 순회하여 최대 거리값을 만든다.
+        for (int i = 0; i < targets.Count; i++)
+        {
+            //물체를 넣어서 경계를 생성한다.
+            bounds.Encapsulate(targets[i].position);
+        }
+        // 플레이어간의 x거리를 반환한다.
+        return bounds.size.x;
+    }
+
+    private Vector2 GetCenterPosition()
+    {
+        //list에 하나만 있다면 처음 타겟의 위치 값을 가져오고
+        if (targets.Count == 1) return targets[0].position;
+
+        //bound를 이용하여 첫 타겟의 중심점을 마우스 위치 가져온다.
+        var bounds = new Bounds(targets[0].position, Vector2.zero);
+        //그게 아니면 리스트를 모두 순회하여
+        for (int i = 0; i < targets.Count; i++)
+        {
+            // 새로운 오브젝트를 넣어 중심 값을 반환한다.
+            bounds.Encapsulate(targets[i].position);
+        }
+        // 중심값을 반환한다.
+
+        return bounds.center;
+    }
+}
