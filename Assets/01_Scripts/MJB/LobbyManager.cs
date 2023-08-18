944489ec49124ac6988fc2ffda1f4612557b6f67
xalswox <qoalswo123@icloud.com>
2023-08-18 17:41:09 +0900

    Feat: Cursor & Camera 멀티플레이 동기화

---
+++ Assets/01_Scripts/MJB/LobbyManager.cs
@@ -0,0 +1,45 @@
+﻿using Cinemachine;
+using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LobbyManager : MonoBehaviour
+{
+    public static LobbyManager instance;
+
+    //나의 커서 photonView 담을 변수
+    public PhotonView cursorPhotonView;
+    public CinemachineVirtualCamera cinemachineVirtualCamera;
+
+    //나 자시자신을 끄는 것을 확인하고 싶다.
+    public RPlayer rplayer;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    //해당 플레이어 선택
+    public void SelectPlayer(PhotonView playerPhoton)
+    {
+        //선택한 플레이어의 포톤을 커서 포톤으로 넘긴다.
+        playerPhoton.TransferOwnership(cursorPhotonView.Owner);
+        //플레이어가 선택되면 커서 비활성화를 동기화한다.
+        cursorPhotonView.RPC("CursorDisable", RpcTarget.All, false);
+        //카메라 타깃 변경
+        cinemachineVirtualCamera.Follow = playerPhoton.transform;
+        //플레이어의 이동 컴포넌트를 가져온다.
+        rplayer = playerPhoton.GetComponent<RPlayer>();
+
+        SetActivePlayer(true);
+    }
+
+    public void SetActivePlayer(bool isEnable)
+    {
+        //내가 취소한 플레이어의 움직임을 끈다.
+        rplayer.enabled = isEnable;
+        //플레이어가 가지고 있는 포톤에서 플레이어가 체크 되어있으면 모든 컴퓨터에 끄는 것을 동기화 한다.
+        rplayer.photonView.RPC("CheckHasPlayer", RpcTarget.All, isEnable);
+    }
+}
---
+++ Assets/01_Scripts/MJB/OwnershipTransfer.cs
@@ -0,0 +1,26 @@
+﻿using Cinemachine;
+using Photon.Pun;
+using UnityEngine;
+
+//해당 플레이어를 클릭했을 때 권한 요청을 수행하여 움직이게 하고 싶다.
+public class OwnershipTransfer : MonoBehaviourPun
+{
+    public bool hasPlayer;
+
+    public void OnClick()
+    {
+        //이미 플레이어가 선택이 되어있으면 선택되지 않게 함수를 나간다.
+        if (hasPlayer == true) return;
+
+        //내가 클릭한 플레이어의 움직임을 활성화 한다.
+        LobbyManager.instance.SelectPlayer(photonView);
+
+    }
+
+    //플레이어를 선택했는지 모든 컴퓨터에 동기화
+    [PunRPC]
+    public void CheckHasPlayer(bool has)
+    {
+        hasPlayer = has;
+    }
+}
