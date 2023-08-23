45ff35ef6bc38803c095b972d5dcc666c0932245
xalswox <qoalswo123@icloud.com>
2023-08-23 17:06:08 +0900

    Add: Cannon Barrel
    플레이어 발사 오브젝트 추가

---
+++ Assets/CannonBarrel.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//플레이어가 캐논에 들어가면 오른쪽 45도 각도로 플레이어를 날리고 싶다.
+public class CannonBarrel : MonoBehaviour
+{
+    //캐논의 발사 위치
+    public Transform fireTransform;
+
+    //플레이어 오브젝트를 담는 변수
+    private GameObject player;
+
+    //변수를 담을 애니메이터
+    public Animator fireAnimator;
+
+    public GameObject SmokeAnimator;
+
+    //발사 힘
+    public float force = 10f;
+
+    public void FirePlayer()
+    {
+        player.gameObject.SetActive(true);
+        //플레이어의 리지드 바디에 폭발적인 힘을 준다.
+        player.GetComponent<Rigidbody>().AddForce(fireTransform.right * force, ForceMode.Impulse);
+        //smokeAnimator를 활성화 하자
+        SmokeAnimator.SetActive(true);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //플레이어가 캐논에 닿으면 플레이어를 오브젝트를 담는다.
+        if (other.CompareTag("Player"))
+        {
+            player = other.gameObject;
+            //플레이어를 잠시 비활성화 한다.
+            other.gameObject.SetActive(false);
+            //플레이어의 위치를 발사 위치로 이동시킨다.
+            other.transform.position = fireTransform.position;
+            fireAnimator.SetTrigger("Fire");
+            //smokeAnimator를 비활성화 하자
+            SmokeAnimator.SetActive(false);
+        }
+    }
+}
