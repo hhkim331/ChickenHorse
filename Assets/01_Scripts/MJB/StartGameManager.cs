bda8a2aeea74deec375bc04b439b3cb7836f70cf
xalswox <qoalswo123@icloud.com>
2023-08-27 09:49:24 +0900

    Add: Timer Counting 배경 이미지 & 카운팅 사운드

--- Assets/01_Scripts/MJB/StartGameManager.cs
+++ Assets/01_Scripts/MJB/StartGameManager.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 //타이머 UI
 //시간이 지남에 따라 텍스트 UI 바뀜
@@ -13,11 +14,13 @@ public class StartGameManager : MonoBehaviourPun
     //시간을 증가시키는 카운트 다운을 만들고 싶다.
     public TextMeshProUGUI timerUI;
 
+    public Image timerBackground;
+
     //3초 시간이 제한
-    private readonly float MAX_TIME = 3.4f, LIMIT_TIME = 0.7f;
+    private readonly float MAX_TIME = 3.4f, LIMIT_TIME = 0f;
 
     private readonly int NEXT_SCENE_NUMBER = 2;
-    private readonly string START_GAME_TEXT = "게임 시작까지\n", HORSE_TAG_NAME = "Horse", CHICKEN_TAG_NAME = "Chicken", DEMICAL_POINT_TRUNCATION = "0";
+    private readonly string START_GAME_TEXT = "파티 게임\n", DEMICAL_POINT_TRUNCATION = "0";
 
     //3초 시간이 되었다는 체크, 플레이어가 들어갔나요?
     private bool hasLimit, hasPlayer;
@@ -26,8 +29,7 @@ public class StartGameManager : MonoBehaviourPun
     private float currentTime;
 
     // 딕셔너리를 사용하여 플레이어를 추가한다.
-    //private Dictionary<string, GameObject> playerTag = new Dictionary<string, GameObject>();
-    int playerCount = 0;
+    private int playerCount = 0;
 
     private bool load = false;
 
@@ -39,6 +41,7 @@ public class StartGameManager : MonoBehaviourPun
         PhotonNetwork.SerializationRate = 30;
         //나 자신을 끈다.
         timerUI.enabled = false;
+        timerBackground.enabled = false;
         // 처음 시간을 초기화
         currentTime = MAX_TIME;
     }
@@ -58,11 +61,11 @@ public class StartGameManager : MonoBehaviourPun
         if (playerCount == PhotonNetwork.CurrentRoom.PlayerCount)
             ReadyToPlay(true);
 
-        //딕셔너리에 플레이어 키가 없다면 UI를 끈다.
-        //if (!playerTag.ContainsKey(HORSE_TAG_NAME) || !playerTag.ContainsKey(CHICKEN_TAG_NAME))
         if (playerCount < PhotonNetwork.CurrentRoom.PlayerCount)
         {
             ReadyToPlay(false);
+            //sfx 사운드를 끈다.
+            SoundManager.Instance.StopSFX("CountStart");
             //시간을 3초로 초기화 한다.
             currentTime = MAX_TIME;
         }
@@ -73,6 +76,7 @@ public class StartGameManager : MonoBehaviourPun
         hasPlayer = isStarting;
         //시간UI 비활성화 한다.
         timerUI.enabled = isStarting;
+        timerBackground.enabled = isStarting;
     }
 
     private void SetCountDown()
@@ -82,12 +86,14 @@ public class StartGameManager : MonoBehaviourPun
         {
             //시간을 증가시켜 문자열로 변환한다.
             currentTime -= Time.deltaTime;
-            //만약에 시간이 1초가 되었다면?
+            SoundManager.Instance.PlaySFXOnce("CountStart");
+
             if (currentTime < LIMIT_TIME)
             {
                 //시간이 지났다는 체크를 한다.
                 hasLimit = true;
                 timerUI.enabled = false;
+                timerBackground.enabled = false;
             }
 
             SetTimerText();
@@ -96,7 +102,9 @@ public class StartGameManager : MonoBehaviourPun
 
     private void SetTimerText()
     {
-        string numberText = currentTime.ToString(DEMICAL_POINT_TRUNCATION);
+        //문자열에 변수를 넣고 싶다면 ""앞에 $를 붙인다.
+        string numberText = $"<size=270>{currentTime.ToString(DEMICAL_POINT_TRUNCATION)}</size>";
+
         //UI에 시간을 넣는다.
         timerUI.text = START_GAME_TEXT + numberText;
     }
@@ -112,9 +120,9 @@ public class StartGameManager : MonoBehaviourPun
                 PhotonNetwork.RemoveRPCs(player);
             PhotonNetwork.LoadLevel(NEXT_SCENE_NUMBER);
         }
-        //SceneManager.LoadScene(NEXT_SCENE_NUMBER);
         //시간 UI 비활성화 한다.
         timerUI.enabled = false;
+        timerBackground.enabled = false;
     }
 
     // 말 플레이어가 태그 되었다면 그 플레이어를 넣는다.
@@ -122,20 +130,13 @@ public class StartGameManager : MonoBehaviourPun
 
     private void OnTriggerEnter(Collider other)
     {
-
         //말이 들어갔다면 말을 넣는다.
         if (other.CompareTag("Player")) playerCount++;
-        ////닭이 들어갔다면 닭을 넣는다.
-        //if (other.CompareTag("Player")) playerTag[CHICKEN_TAG_NAME] = other.gameObject;
     }
 
     //플레이어가 나갔을 때
     private void OnTriggerExit(Collider other)
     {
         if (other.CompareTag("Player")) playerCount--;
-        ////말이 나갔다면 말을 뺀다.
-        //if (other.CompareTag(HORSE_TAG_NAME)) playerTag.Remove(HORSE_TAG_NAME);
-        ////닭이 나갔다면 닭을 뺀다.
-        //if (other.CompareTag(CHICKEN_TAG_NAME)) playerTag.Remove(CHICKEN_TAG_NAME);
     }
 }
