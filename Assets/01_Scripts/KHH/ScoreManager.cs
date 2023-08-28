424ad8a0f6eb19d8d10301f5ad01d7685fe5e4f9
hhkim331 <97862258+hhkim331@users.noreply.github.com>
2023-08-28 09:59:08 +0900

    z

--- Assets/01_Scripts/KHH/ScoreManager.cs
+++ Assets/01_Scripts/KHH/ScoreManager.cs
@@ -107,6 +107,7 @@ public class ScoreManager : MonoBehaviourPun
         for (int i = 0; i < actors.Count; i++)
         {
             PlayerInfo info = Instantiate(playerInfoFactory, playerInfoParent).GetComponent<PlayerInfo>();
+            info.transform.SetAsLastSibling();
             info.Set(actors[i].Item2, PlayerData.instance.PlayerCharacterDic[actors[i].Item1]);
             playerInfos.Add(actors[i].Item1, info);
             playerScore.Add(actors[i].Item1, 0);
