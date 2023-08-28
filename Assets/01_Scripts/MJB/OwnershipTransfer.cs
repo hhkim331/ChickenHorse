963c3f4d8c4272d483c6d944f9642a1c01ddf985
xalswox <qoalswo123@icloud.com>
2023-08-28 09:46:43 +0900

    Fix: Player 선택 후 마우스 이동시 다른 플레이어 선택 되는 이슈
    플레이어 선택 후 다른 플레이어 선택 불가능으로 변경

--- Assets/01_Scripts/MJB/OwnershipTransfer.cs
+++ Assets/01_Scripts/MJB/OwnershipTransfer.cs
@@ -1,6 +1,7 @@
 ﻿using Photon.Pun;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 //해당 플레이어를 클릭했을 때 권한 요청을 수행하여 움직이게 하고 싶다.
 public class OwnershipTransfer : MonoBehaviourPun
@@ -53,6 +54,7 @@ public class OwnershipTransfer : MonoBehaviourPun
     {
         //이미 플레이어가 선택이 되어있으면 선택되지 않게 함수를 나간다.
         if (hasPlayer == true) return;
+        if (LobbyManager.instance.rPlayer != null) return;
 
         //내가 클릭한 플레이어의 움직임을 활성화 한다.
         LobbyManager.instance.SelectPlayer(photonView);
